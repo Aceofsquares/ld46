8993895800c93e09fa74b231f603faf9cad4cd51
Aceofsquares <btcr1990@gmail.com>
2020-04-19 21:49:28 -0500

    Apply stats based on die

--- Scenes/slime/faceholder.gd
+++ Scenes/slime/faceholder.gd
@@ -5,22 +5,19 @@ export(Array, PackedScene) var faces = []
 
 const MOOD = preload("res://Scenes/slime/mood_enum.gd")
 
-var current_mood
+var current_mood setget set_mood
 var face_animation
 
 
 func _ready():
-	current_mood = MOOD.Mood.HAPPY
-	set_face(current_mood)
+	self.current_mood = MOOD.Mood.HAPPY
 	
-func _input(event):
-	if event.is_action_pressed("change_face"):
-		current_mood += 1
-		current_mood %= MOOD.Mood.size()
-		set_face(current_mood)
+#func _input(event):
+#	if event.is_action_pressed("change_face"):
+#		current_mood += 1
+#		set_face(current_mood)
 
-
-func set_face(mood):
+func _process(_delta):
 	if face_animation:
 		face_animation.visible = false
 	match current_mood:
@@ -35,3 +32,7 @@ func set_face(mood):
 		MOOD.Mood.DEAD:
 			face_animation = $DeadFace
 	face_animation.visible = true
+
+func set_mood(mood):
+	current_mood = mood
+	
--- Scenes/slime/slimagotchi.gd
+++ Scenes/slime/slimagotchi.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 signal status_applied(resource)
 
+const MOOD = preload("res://Scenes/slime/mood_enum.gd")
 const _resource = preload("res://Scenes/die/pip_enum.gd")
 
-var hunger = 25
-var thirst = 25
-var boredom = 25
-var energy = 25
+var fullness = 25 setget set_fullness
+var quench = 25 setget set_quench
+var entertained = 25 setget set_entertained
+var energy = 25 setget set_energy
 
+func _ready():
+	change_face()
 
 func apply_stat(resource):
 	print("APPLYING " + _resource.pip_text(resource))
@@ -16,13 +19,94 @@ func apply_stat(resource):
 		_resource.EMPTY:
 			pass
 		_resource.WATER:
-			pass
+			drink_water()
 		_resource.FOOD:
-			pass
+			eat_food()
 		_resource.SLEEP:
-			pass
+			sleep()
 		_resource.ENTERTAINMENT:
-			pass
+			be_entertained()
 		_resource.SALT:
-			pass
+			take_salt_damage()
+	print("My Stats Are")
+	print("FOOD: %d\nQUENCHED: %d\nENTER: %d\nENERGY: %d" % [fullness, quench, entertained, energy])
+	change_face()
 	emit_signal("status_applied", resource)
+
+func eat_food():
+	self.fullness += 4
+	self.quench -= 2
+	self.entertained -= 3
+	self.energy += 1
+
+func drink_water():
+	self.quench += 5
+	self.entertained -= 3
+	self.energy -= 2
+
+func be_entertained():
+	self.fullness -= 1
+	self.quench -= 1
+	self.entertained += 5
+	self.energy -= 3
+
+func sleep():
+	self.fullness -= 3
+	self.quench -= 2
+	self.entertained += 1
+	self.energy += 4
+
+
+func take_salt_damage():
+	var resource_die = randi() % 4
+	var damage_die = (randi() % 5) + 1
+	if resource_die == 0:
+		self.fullness -= damage_die
+	elif resource_die == 1:
+		self.quench -= damage_die
+	elif resource_die == 2:
+		self.entertained -= damage_die
+	elif resource_die == 3:
+		self.energy -= damage_die
+
+func set_fullness(value):
+	fullness = value
+	if fullness > 25:
+		fullness = 25
+	if fullness < 0:
+		fullness = 0
+
+func set_quench(value):
+	quench = value
+	if quench > 25:
+		quench = 25
+	if quench < 0:
+		quench = 0
+
+func set_entertained(value):
+	entertained = value
+	if entertained > 25:
+		entertained = 25
+	if entertained < 0:
+		entertained = 0
+
+func set_energy(value):
+	energy = value
+	if energy > 25:
+		energy = 25
+	if energy < 0:
+		energy = 0
+
+func change_face():
+	var sum = fullness + quench + entertained + energy
+	var avg = sum / 4
+	if avg > 20:
+		$FaceHolder.set_mood(MOOD.Mood.HAPPY)
+	elif avg > 15:
+		$FaceHolder.set_mood(MOOD.Mood.MEH)
+	elif avg > 10:
+		$FaceHolder.set_mood(MOOD.Mood.SAD)
+	elif avg > 0:
+		$FaceHolder.set_mood(MOOD.Mood.MAD)
+	else:
+		$FaceHolder.set_mood(MOOD.Mood.DEAD)
